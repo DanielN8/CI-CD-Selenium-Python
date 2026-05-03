9f268c95337db07359be0a5240568b46131df80b
DanielN <daniel.nie@utp.ac.pa>
2026-05-03 15:43:15 -0500

    Lab done

--- 2_robust_test.py
+++ 2_robust_test.py
@@ -2,10 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 # 1. Configuración del Driver
-driver = webdriver.Chrome()
-# Definimos una espera máxima de 10 segundos
+options = Options()
+options.add_argument("--headless") # Ejecución sin ventana 
+options.add_argument("--no-sandbox") 
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=options)    # Definimos una espera máxima de 10 segundos
 wait = WebDriverWait(driver, 10)
 
 try:
